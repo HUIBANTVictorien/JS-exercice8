f434bdde1bd810f86fc67c91457724f62bdd2f38
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-10-26 09:53:34 +0200

    ajout condition si value != nombre

--- exercice8.js
+++ exercice8.js
@@ -1,9 +1,13 @@
-function display() {
-  var old = document.getElementById('age').value;
-  if(old >= 18) {
-    alert('Vous êtes majeur');
-  }
-  else {
-    alert('Vous êtes mineur');
-  }
-};
+
+function display(){
+    var old = document.getElementById('age').value;
+    if (isNaN(old) == false && old > 0){
+      if (old < 18){
+        alert('Vous êtes mineur');
+      } else{
+        alert('Vous êtes majeur');
+      };
+    } else {
+      alert('Rentrer un âge valide');
+    };
+  };
